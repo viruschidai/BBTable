73459048e490e7cc89a408dbddec145bfe83f7af
Bill Gao <viruschidai@gmail.com>
2013-09-24 23:45:44 +1000

    add editable

--- src/BBTable.js
+++ src/BBTable.js
@@ -7,24 +7,26 @@
 
   if (!this.BBLib) {
     BBLib = this.BBLib = {}
-    BBLib.VERSION = "0.1.0";
+    BBLib.VERSION = '0.1.0';
   } else {
     BBLib = this.BBLib;
   }
 
+  var events = _.extend({}, Backbone.Events)
+
   var requiredOptions = function(options, required) {
-    for (var x in required) {
-      var option = required[x];
+    required.forEach(function(option) {
       if (!(option in options)) {
-        throw new Error("option." + option + " is required!");
+        throw new Error('option.' + option + ' is required!');
       }
-    }
+    });
   };
 
+  // A model that controls sorting
   var SortingModel = BBLib.SortingModel = Backbone.Model.extend({
     defaults: {
       sortDir: 'asc',
-      sortKey: "id",
+      sortKey: 'id',
       comparator: null
     },
 
@@ -35,7 +37,6 @@
         default: return 'unsorted';
       }
     }
-
   });
 
   function getDefaultComparator(key, dir) {
@@ -44,11 +45,11 @@
           b = right.get(key);
 
       if (a > b) {
-        return dir === "asc" ? 1 : -1;
+        return dir === 'asc' ? 1 : -1;
       }
 
       if (a < b){
-        return dir === "desc" ? 1 : -1;
+        return dir === 'desc' ? 1 : -1;
       }
 
       return 0;
@@ -62,11 +63,11 @@
       this.cachedResults = new Backbone.Collection();
       this.comparator = options.comparator || getDefaultComparator;
 
-      this.paginationModel.on("change:page change:pageSize", function(e) {
+      this.paginationModel.on('change:page change:pageSize', function(e) {
         this.reset(this._getCurrentPage());
       }, this);
 
-      this.sortingModel.on("change", function(e) {
+      this.sortingModel.on('change', function(e) {
         this.cachedResults.comparator = this.comparator(this.sortingModel.get('sortKey'), this.sortingModel.get('sortDir'));
         this.cachedResults.sort({sort: false});
         this.reset(this._getCurrentPage());
@@ -80,12 +81,12 @@
     },
 
     _getCurrentPage: function() {
-      var currentPage = this.paginationModel.get("page");
+      var currentPage = this.paginationModel.get('page');
       return this._getPage(currentPage);
     },
 
     _getPage: function(page) {
-      var pageSize = this.paginationModel.get("pageSize"),
+      var pageSize = this.paginationModel.get('pageSize'),
           pageStart = page * pageSize;
 
       if (pageSize === 0) {
@@ -101,11 +102,11 @@
       this.paginationModel = options.paginationModel || new PaginationModel();
       this.sortingModel = options.sortingModel || new SortingModel();
 
-      this.paginationModel.on("change:page change:pageSize", function() {
+      this.paginationModel.on('change:page change:pageSize', function() {
         this.fetch();
       }, this);
 
-      this.sortingModel.on("change", function(e) {
+      this.sortingModel.on('change', function(e) {
         this.fetch();
       }, this);
     },
@@ -126,10 +127,21 @@
       key: undefined,
       label: undefined,
       sortable: true,
-      editable: true,
+      editable: false,
       renderable: true,
       cell: Cell,
-      cellEditor: undefined
+      cellEditorClass: undefined
+    },
+
+    onCellEdit: function(cell, model) {
+      var CellEditor = this.get('cellEditorClass') || InputCellEditor
+      new CellEditor({cell: cell, model: model, col: this});
+    },
+
+    initialize: function() {
+      if (this.get('editable')) {
+        this.on('cell:edit', this.onCellEdit, this)
+      }
     }
   });
 
@@ -142,15 +154,15 @@
     },
 
     initialize: function() {
-      this.on("change:numOfRecords change:pageSize", function() {
-        var pageSize = this.get("pageSize"),
-          numOfRecords = this.get("numOfRecords"),
-          numOfPages = this.get("pageSize") === 0 ? (numOfRecords > 0 ? 1 : 0) : Math.ceil(numOfRecords / pageSize);
+      this.on('change:numOfRecords change:pageSize', function() {
+        var pageSize = this.get('pageSize'),
+          numOfRecords = this.get('numOfRecords'),
+          numOfPages = this.get('pageSize') === 0 ? (numOfRecords > 0 ? 1 : 0) : Math.ceil(numOfRecords / pageSize);
 
-        this.set("numOfPages", numOfPages);
+        this.set('numOfPages', numOfPages);
 
-        if (this.get("page") >= numOfPages) {
-          this.set("page", (numOfPages - 1));
+        if (this.get('page') >= numOfPages) {
+          this.set('page', (numOfPages - 1));
         }
       });
     },
@@ -159,41 +171,42 @@
 
     toQueryParams: function() {
       var params = _.extend({}, this.attributes);
-      if (!_.empty(this.queryParamsMap)) {
-        _.each(this.queryParamsMap, function(key, value) {
-          if (! value in this.attributes) {
-            throw new Error(value + " is not a valid property.");
-          };
-          params[key] = this.attributes[value];
-        });
-      };
+
+      if (_.empty(this.queryParamsMap)) return params; 
+
+      _.each(this.queryParamsMap, function(key, value) {
+        if (! value in this.attributes) {
+          throw new Error(value + ' is not a valid property.');
+        };
+        params[key] = this.attributes[value];
+      });
 
       return params;
     },
 
     validate: function(attrs, options) {
       if (attrs.pageSize < 0) {
-          return "Invalid pageSize value [" + attrs.pageSize + "]";
+          return 'Invalid pageSize value [' + attrs.pageSize + ']';
       };
 
       if (attrs.page < 0 || attrs.page > this.get('numOfPages') - 1) {
-          return "Invalid currentPage value [" + attrs.currentPage + "]";
+          return 'Invalid currentPage value [' + attrs.currentPage + ']';
       };
     }
   });
 
   var PageSelectorView = BBLib.PageSelectorView = Backbone.View.extend({
-    tagName: "div",
+    tagName: 'div',
 
     windowSize: 5,
 
-    className: "pagination numbers",
+    className: 'pagination numbers',
 
     fastForwardings: {
-      "first": "<<",
-      "last": ">>",
-      "prev": "<",
-      "next": ">"
+      'first': '<<',
+      'last': '>>',
+      'prev': '<',
+      'next': '>'
     },
 
     template: _.template("<ul class='page-selector'>"
@@ -204,7 +217,7 @@
                 ),
 
     events: {
-      "click .page": "onClickChangePage"
+      'click .page': 'onClickChangePage'
     },
 
     initialize: function(options) {
@@ -222,45 +235,45 @@
     },
 
     getDisplayPages: function() {
-      if(this.model.get("numOfPages") > 0) {
-        var page = this.model.get("page"),
-          numOfPages = this.model.get("numOfPages"),
-          startPage = Math.max(1, Math.min(page + 1 - parseInt(this.windowSize/2), numOfPages + 1 - this.windowSize)),
-          endPage = Math.min(this.model.get("numOfPages")+1, startPage + this.windowSize);
 
-        var result = [
-          {label: this.fastForwardings.first, cssClass: page == 0 ? "disabled" : ""},
-          {label: this.fastForwardings.prev, cssClass: page == 0 ? "disabled" : ""},
-        ];
+      if(!(this.model.get('numOfPages') > 0)) return []
 
-        var range = _.range(startPage, endPage, 1);
-        _.each(range, function(pageNum) {
-          result.push({label: pageNum, cssClass: page + 1 === pageNum ? "active" : ""});
-        });
+      var page = this.model.get('page'),
+        numOfPages = this.model.get('numOfPages'),
+        startPage = Math.max(1, Math.min(page + 1 - parseInt(this.windowSize/2), numOfPages + 1 - this.windowSize)),
+        endPage = Math.min(this.model.get('numOfPages')+1, startPage + this.windowSize);
 
-        result = result.concat([
-            {label: this.fastForwardings.next, cssClass: page + 1 == numOfPages ? "disabled" : ""},
-            {label: this.fastForwardings.last, cssClass: page + 1 == numOfPages ? "disabled" : ""},
-            ]);
+      var result = [
+        {label: this.fastForwardings.first, cssClass: page == 0 ? 'disabled' : ''},
+        {label: this.fastForwardings.prev, cssClass: page == 0 ? 'disabled' : ''},
+      ];
 
-        return result;
-      } else {
-        return [];
-      }
+      var range = _.range(startPage, endPage, 1);
+
+      _.each(range, function(pageNum) {
+        result.push({label: pageNum, cssClass: page + 1 === pageNum ? 'active' : ''});
+      });
+
+      return result.concat([
+          {label: this.fastForwardings.next, cssClass: page + 1 == numOfPages ? 'disabled' : ''},
+          {label: this.fastForwardings.last, cssClass: page + 1 == numOfPages ? 'disabled' : ''},
+        ]);
+      
     },
 
     onClickChangePage: function(ev) {
       ev.preventDefault();
       ev.stopPropagation();
+
       var pageStr = $(ev.target).text().trim();
       var pageIndex = this.convertPageIndex(pageStr);
-      this.model.set("page", pageIndex, {validate: true});
+      this.model.set('page', pageIndex, {validate: true});
     },
 
     convertPageIndex: function(pageStr) {
       var page = 0,
-      page = this.model.get("page"),
-      numOfPages = this.model.get("numOfPages");
+      page = this.model.get('page'),
+      numOfPages = this.model.get('numOfPages');
 
       switch(pageStr){
         case this.fastForwardings.first:
@@ -285,13 +298,13 @@
   });
 
   var GotoPageView = BBLib.GotoPageView= Backbone.View.extend({
-    tagName: "div",
+    tagName: 'div',
 
-    className: "pagination goto",
+    className: 'pagination goto',
 
     events: {
-      "blur input": "changePage",
-      "keypress input": "changePageOnEnter"
+      'blur input': 'changePage',
+      'keypress input': 'changePageOnEnter'
     },
 
     template: _.template("Goto <input type='text' value='<%= page + 1 %>'/> of <%= numOfPages %>"),
@@ -310,10 +323,11 @@
     changePage: function(ev) {
       var pageStr = $(ev.target).val();
       var pageIndex = +pageStr - 1;
+
       if (_.isNaN(pageIndex) || pageIndex <= 0 || (pageIndex > (this.model.get('numOfPages') - 1))) {
         $(ev.target).val(this.model.get('page') + 1);
       };
-      this.model.set("page", pageIndex, {validate: true});
+      this.model.set('page', pageIndex, {validate: true});
     },
 
     changePageOnEnter: function(ev) {
@@ -323,8 +337,8 @@
   });
 
   var PageSizeSelectorView = BBLib.PageSizeSelectorView = Backbone.View.extend({
-    tagName: "div",
-      className: "pagesize-selector",
+    tagName: 'div',
+      className: 'pagesize-selector',
 
       pageSizeOptions: ['All', 10, 20],
 
@@ -338,7 +352,7 @@
         + "</div>"),
 
       events: {
-        "click .page-size": "onClickChangePageSize"
+        'click .page-size': 'onClickChangePageSize'
       },
 
       initialize: function() {
@@ -350,13 +364,13 @@
         var pageSizeStr = $(ev.target).text().trim();
         var pageSize = this.convertPageSize(pageSizeStr);
 
-        this.model.set("pageSize", pageSize);
+        this.model.set('pageSize', pageSize);
       },
 
       convertPageSize: function(pageSizeStr) {
         var pageSize = 0;
 
-        if (pageSizeStr == "All") {
+        if (pageSizeStr == 'All') {
           pageSize = 0;
         } else {
           pageSize = parseInt(pageSizeStr);
@@ -367,11 +381,11 @@
 
       render: function() {
         this.$el.empty();
-        var pageSize = this.model.get("pageSize");
+        var pageSize = this.model.get('pageSize');
 
         var pageSizeInfo = {
           pageSizes: this.pageSizeOptions,
-          pageSize: pageSize === 0 ? "All" : pageSize
+          pageSize: pageSize === 0 ? 'All' : pageSize
         };
 
         this.$el.html(this.template(pageSizeInfo));
@@ -381,9 +395,9 @@
 
 
   var PaginatorView = BBLib.PaginatorView = Backbone.View.extend({
-    tagName: "div",
+    tagName: 'div',
 
-    className: "paginator",
+    className: 'paginator',
 
     initialize: function(options) {
       this.pageSelector = new PageSelectorView({model: this.model});
@@ -393,9 +407,11 @@
     render: function() {
       this.$el.empty();
       this.$el.append(this.pageSelector.render().el);
+
       var gotoEl = this.gotoPage.render().el;
-      $(gotoEl).addClass("pull-right");
+      $(gotoEl).addClass('pull-right');
       this.$el.append(this.gotoPage.render().el);
+
       return this;
     }
   });
@@ -406,25 +422,23 @@
   });
 
   var Cell = BBLib.Cell = Backbone.View.extend({
-    tagName: "td",
+    tagName: 'td',
 
-    className: "bbtable-cell",
+    className: 'bbtable-cell',
 
     events: {
-      "click, dbclick": function(e) {
-        this.trigger(event, this.model, this.col);
-      }
+      'dblclick': 'onClick'
     },
-
 
     initialize: function(options){
       requiredOptions(options, ['col']);
       this.col = options.col;
+      _.bindAll(this, 'render', 'onClick');
     },
 
     render: function() {
       this.$el.html(this.fromRaw());
-      this.$el.addClass("col-" + this.col.get("key"));
+      this.$el.addClass('col-' + this.col.get('key'));
       return this;
     },
 
@@ -436,15 +450,20 @@
     fromRaw: function() {
       var v = this.model.get(this.col.get('key'));
       return v;
+    },
+
+    onClick: function() {
+      this.col.trigger('cell:edit', this.$el, this.model);
     }
   });
 
   var HeaderCell = BBLib.HeaderCell = Backbone.View.extend({
-    tagName: "td",
+    tagName: 'td',
 
-    className: "bbtable-header-cell",
+    className: 'bbtable-header-cell',
 
-    sortingTemplate: _.template("<div class='sorting sorting-<%= sortDir %> pull-right'><i class='<%= iconClass %>'></i></div>"),
+    sortingTemplate: _.template("<div class='sorting sorting-<%= sortDir %> pull-right'>" 
+      + "<i class='<%= iconClass %>'></i></div>"),
 
     events: {
       'click .sorting': 'sort'
@@ -461,11 +480,13 @@
 
     render: function() {
       this.$el.html(this.model.get('label') || this.model.get('key'));
-      this.$el.addClass("col-" + this.model.get("key"));
+      this.$el.addClass('col-' + this.model.get('key'));
+
       if (this.model.get('sortable')) {
-        this.$el.addClass("sortable");
+        this.$el.addClass('sortable');
         this.renderSortingEl()
       }
+
       return this;
     },
 
@@ -500,22 +521,23 @@
 
     _getSortingIconClass: function(sortDir) {
       switch (sortDir) {
-          case "asc": return "icon-chevron-up";
-          case "desc": return "icon-chevron-down";
-          default: return "icon-unsorted";
+          case 'asc': return 'icon-chevron-up';
+          case 'desc': return 'icon-chevron-down';
+          default: return 'icon-unsorted';
       }
     }
   });
 
   var TableHeader = BBLib.TableHeader = Backbone.View.extend({
-    tagName: "div",
+    tagName: 'div',
 
     template: _.template("<table class='table'></table>"),
 
-    className: "bbtable-header",
+    className: 'bbtable-header',
 
     initialize: function(options) {
       _.bindAll(this, 'render');
+      
       this.cols = options.cols;
       this.headerCellClass = options.headerCellClass || HeaderCell;
     },
@@ -524,25 +546,28 @@
       this.$el.empty();
       this.$el.html(this.template());
       this.$table = this.$('table');
-      var row = $("<tr></tr>");
+      var row = $('<tr></tr>');
+
       _.each(this.cols.models, function(col) {
         var CellClass= col.headerCell || HeaderCell;
         var cell = new CellClass({model: col, collection: this.model});
         row.append(cell.render().el);
       }, this);
       this.$table.append(row);
+
       return this;
     }
   });
 
   var Table = BBLib.Table = Backbone.View.extend({
-    tagName: "div",
+    tagName: 'div',
 
-    className: "bbtable",
+    className: 'bbtable',
 
     initialize: function(options) {
       requiredOptions(options, ['cols']);
-      this.editable = options.editable || false
+
+      this.editable = options.editable || false;
       this.headClass = options.headClass || TableHeader;
       this.bodyClass = options.bodyClass || TableBody;
       this.head = new this.headClass(options);
@@ -558,16 +583,17 @@
   });
 
   var TableBody = BBLib.TableBody = Backbone.View.extend({
-    tagName: "div",
+    tagName: 'div',
 
-    className: "bbtable-body",
+    className: 'bbtable-body',
 
-    template: _.template("<table class='table'></table>"),
+    template: _.template("<table class='table table-striped'></table>"),
 
     initialize: function(options) {
       requiredOptions(options, ['cols']);
       _.bindAll(this, 'render');
-      this.model.on("reset", this.render);
+
+      this.model.on('reset', this.render);
       this.rowClass = options.rowClass || TableRow;
       this.cols = options.cols;
       this.rows = [];
@@ -578,26 +604,32 @@
       this.$el.html(this.template());
       this.$table = this.$('table');
       this.rows = [];
+
       _.each(this.model.models, function(m) {
         var row = new this.rowClass({model: m, cols: this.cols});
         this.rows.push(row);
         this.$table.append(row.render().el);
       }, this);
+      
       return this;
     }
   });
 
   var TableRow = BBLib.TableRow = Backbone.View.extend({
-    tagName: "tr",
+    tagName: 'tr',
 
-    className: "bbtable-row",
+    className: 'bbtable-row',
 
     initialize: function(options) {
       requiredOptions(options, ['cols']);
       this.cols = options.cols;
+      _.bindAll(this, 'render');
+
+      this.listenTo(this.model, 'change', this.render);
     },
 
     render: function() {
+      this.$el.empty();
       _.each(this.cols.models, function(col){
         var CellClass= col.cell || Cell;
         var v = new CellClass({model: this.model, col: col});
@@ -607,11 +639,70 @@
     }
   });
 
+  var CellEditor = BBLib.CellEditor = Backbone.View.extend({
+    tagName: 'div',
+    
+    className: 'cell-editor'
+  });
+  
+  var InputCellEditor = BBLib.InputCellEditor = CellEditor.extend({
+    events: {
+      'blur input': 'blur',
+      'keypress input': 'keypress'
+    },
+    
+    template: _.template("<input type='text' value=<%= value %>>"),
+    
+    initialize: function(options) {
+      this.model = options.model;
+      this.cell = options.cell;
+      this.col = options.col;
+      
+      this.render();
+      $(document.body).append(this.el);
+      this.$input = this.$el.find('input');
+      this.show();
+    },
+    
+    render: function() {
+      this.$el.append(this.template({value: this.model.get(this.col.get('key'))}));
+      return this;
+    },
+    
+    keypress: function(ev) {
+      if(ev.keyCode === 13) {
+        this.updateModel();
+        this.remove();
+      } else if (ev.keyCode === 27) {
+        this.remove();
+      }
+    },
+    
+    show: function() {
+      this.$el.offset(this.cell.offset());
+      this.$input.outerHeight(this.cell.outerHeight());
+      this.$input.outerWidth(this.cell.outerWidth());
+      
+      this.$input.focus();
+      this.$input.select();
+    },
+    
+    // Update model with edited value
+    updateModel: function() {
+      this.model.set(this.col.get('key'), this.$input.val());
+    },
+    
+    blur: function() {
+      this.updateModel();
+      this.remove();
+    }
+  });
+
   $.fn.bbtable = function(options) {
-    requiredOptions(['url', 'columns'])
+    requiredOptions(options, ['url', 'columns'])
 
     options = $.extend({
-      modelCls: Backbone.Model,
+      modelClass: Backbone.Model,
       pagingMode: 'client', // possible value 'client' or 'server'
       pageSize: 100,
       paginatorContainer: null,
@@ -627,7 +718,7 @@
 
       var cols = new Columns(options.columns),
           pageableCls = options.pagingMode === 'client' ? ClientSidePageable : ServerSidePageable,
-          Collection = pageableCls.extend({url: options.url, model: options.modelCls}),
+          Collection = pageableCls.extend({url: options.url, model: options.modelClass}),
           collection = new Collection({paginationModel: paginationModel}),
           table = new Table({cols: cols, model: collection}),
           paginator = new options.paginatorCls({model: paginationModel});
